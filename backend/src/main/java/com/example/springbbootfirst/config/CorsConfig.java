86bf17cfba4b1ad37ec86a2e4291a7f928ddbc57
sriramkv1409 <sriramkv1409@gmail.com>
2025-07-25 21:35:17 +0530

    corse error1

--- backend/src/main/java/com/example/springbbootfirst/config/CorsConfig.java
+++ backend/src/main/java/com/example/springbbootfirst/config/CorsConfig.java
@@ -11,7 +11,7 @@ public class CorsConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry){
         registry.addMapping("/**")
                 .allowedOrigins("https://team-sync-omega.vercel.app/")
-                .allowedMethods("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*")
                 .allowCredentials(true);
     }
