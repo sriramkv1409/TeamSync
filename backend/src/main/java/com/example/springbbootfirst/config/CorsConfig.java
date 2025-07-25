e7d12b185583435e0da813f49a154da99b97180a
sriramkv1409 <sriramkv1409@gmail.com>
2025-07-25 22:01:39 +0530

    cors error

--- backend/src/main/java/com/example/springbbootfirst/config/CorsConfig.java
+++ backend/src/main/java/com/example/springbbootfirst/config/CorsConfig.java
@@ -10,7 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry){
         registry.addMapping("/**")
-                .allowedOrigins("https://team-sync-omega.vercel.app/")
+                .allowedOrigins("https://team-sync-omega.vercel.app")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*")
                 .allowCredentials(true);
