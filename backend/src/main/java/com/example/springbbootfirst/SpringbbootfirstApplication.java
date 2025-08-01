e1357aa58a5bd60e61e755225a15ff087eaa93a6
sriramkv1409 <sriramkv1409@gmail.com>
2025-08-01 21:02:41 +0530

    env fixed

--- backend/src/main/java/com/example/springbbootfirst/SpringbbootfirstApplication.java
+++ backend/src/main/java/com/example/springbbootfirst/SpringbbootfirstApplication.java
@@ -12,21 +12,12 @@ public class SpringbbootfirstApplication {
 
 	public static void main(String[] args) {
 		Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
-		System.setProperty("SPRING_APPLICATION_NAME", dotenv.get("SPRING_APPLICATION_NAME"));
-        System.setProperty("SPRING_DATASOURCE_DRIVER_CLASS_NAME",dotenv.get("SPRING_DATASOURCE_DRIVER_CLASS_NAME"));
-		System.setProperty("SPRING_DATASOURCE_URL", dotenv.get("SPRING_DATASOURCE_URL"));
-		System.setProperty("SPRING_DATASOURCE_USERNAME", dotenv.get("SPRING_DATASOURCE_USERNAME"));
-		System.setProperty("SPRING_DATASOURCE_PASSWORD", dotenv.get("SPRING_DATASOURCE_PASSWORD"));
-		System.setProperty("SPRING_JPA_SHOW_SQL", dotenv.get("SPRING_JPA_SHOW_SQL"));
-		System.setProperty("SPRING_JPA_HIBERNATE_DDL_AUTO", dotenv.get("SPRING_JPA_HIBERNATE_DDL_AUTO"));
-		System.setProperty("SPRING_JPA_PROPERTIES_HIBERNATE_DIALECT", dotenv.get("SPRING_JPA_PROPERTIES_HIBERNATE_DIALECT"));
-		System.setProperty("SPRING_SECURITY_USER_NAME", dotenv.get("SPRING_SECURITY_USER_NAME"));
-		System.setProperty("SPRING_SECURITY_USER_PASSWORD", dotenv.get("SPRING_SECURITY_USER_PASSWORD"));
-		System.setProperty("SPRING_SECURITY_USER_ROLES", dotenv.get("SPRING_SECURITY_USER_ROLES"));
-		System.setProperty("APP_JWT_SECRET", dotenv.get("APP_JWT_SECRET"));
-		System.setProperty("APP_JWT_EXPIRATION_MILLISECONDS", dotenv.get("APP_JWT_EXPIRATION_MILLISECONDS"));
-		System.setProperty("EMAIL",dotenv.get("EMAIL"));
-		System.setProperty("APP_PASSWORD",dotenv.get("APP_PASSWORD"));
+
+		dotenv.entries().forEach(entry -> {
+			if (entry.getValue() != null) {
+				System.setProperty(entry.getKey(), entry.getValue());
+			}
+		});
 		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Kolkata"));
 		SpringApplication.run(SpringbbootfirstApplication.class, args);
 	}
